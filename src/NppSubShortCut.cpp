5d8edfbde24b74215b05e8fbc8836bae5e11348c
beshi <beshicom@outlook.jp>
2023-09-10 11:45:29 +0900

    first commit

---
+++ src/NppSubShortCut.cpp
@@ -0,0 +1,93 @@
+//
+//SubShortCut
+//this file is part of notepad++
+//
+
+#include "PluginDefinition.h"
+
+extern FuncItem funcItem[nbFunc];
+extern NppData nppData;
+
+
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD  reasonForCall, LPVOID /*lpReserved*/)
+{
+	try {
+
+		switch (reasonForCall)
+		{
+			case DLL_PROCESS_ATTACH:
+				pluginInit(hModule);
+				break;
+
+			case DLL_PROCESS_DETACH:
+				pluginCleanUp();
+				break;
+
+			case DLL_THREAD_ATTACH:
+				break;
+
+			case DLL_THREAD_DETACH:
+				break;
+		}
+	}
+	catch (...) { return FALSE; }
+
+    return TRUE;
+}
+
+
+extern "C" __declspec(dllexport) void setInfo(NppData notpadPlusData)
+{
+	nppData = notpadPlusData;
+	commandMenuInit();
+}
+
+extern "C" __declspec(dllexport) const TCHAR * getName()
+{
+	return NPP_PLUGIN_NAME;
+}
+
+extern "C" __declspec(dllexport) FuncItem * getFuncsArray(int *nbF)
+{
+	*nbF = nbFunc;
+	return funcItem;
+}
+
+
+extern "C" __declspec(dllexport) void beNotified(SCNotification *notifyCode)
+{
+	switch (notifyCode->nmhdr.code) 
+	{
+		case NPPN_SHUTDOWN:
+		{
+			commandMenuCleanUp();
+		}
+		break;
+
+		default:
+			return;
+	}
+}
+
+
+// Here you can process the Npp Messages 
+// I will make the messages accessible little by little, according to the need of plugin development.
+// Please let me know if you need to access to some messages :
+// http://sourceforge.net/forum/forum.php?forum_id=482781
+//
+extern "C" __declspec(dllexport) LRESULT messageProc(UINT /*Message*/, WPARAM /*wParam*/, LPARAM /*lParam*/)
+{/*
+	if (Message == WM_MOVE)
+	{
+		::MessageBox(NULL, "move", "", MB_OK);
+	}
+*/
+	return TRUE;
+}
+
+#ifdef UNICODE
+extern "C" __declspec(dllexport) BOOL isUnicode()
+{
+    return TRUE;
+}
+#endif //UNICODE
---
+++ src/PluginDefinition.cpp
@@ -0,0 +1,104 @@
+//
+//SubShortCut
+//this file is part of notepad++
+//
+
+#include "PluginDefinition.h"
+#include "menuCmdID.h"
+
+//
+// The plugin data that Notepad++ needs
+//
+FuncItem funcItem[nbFunc];
+
+//
+// The data of Notepad++ that you can use in your plugin commands
+//
+NppData nppData;
+
+//
+// Initialize your plugin data here
+// It will be called while plugin loading   
+void pluginInit(HANDLE /*hModule*/)
+{
+}
+
+//
+// Here you can do the clean up, save the parameters (if any) for the next session
+//
+void pluginCleanUp()
+{
+}
+
+//
+// Initialization of your plugin commands
+// You should fill your plugins commands here
+void commandMenuInit()
+{
+
+    //--------------------------------------------//
+    //-- STEP 3. CUSTOMIZE YOUR PLUGIN COMMANDS --//
+    //--------------------------------------------//
+    // with function :
+    // setCommand(int index,                      // zero based number to indicate the order of command
+    //            TCHAR *commandName,             // the command name that you want to see in plugin menu
+    //            PFUNCPLUGINCMD functionPointer, // the symbol of function (function pointer) associated with this command. The body should be defined below. See Step 4.
+    //            ShortcutKey *shortcut,          // optional. Define a shortcut to trigger this command
+    //            bool check0nInit                // optional. Make this menu item be checked visually
+    //            );
+    setCommand(0, TEXT("SubShortCut"), SubShortCut, NULL, false);
+}
+
+//
+// Here you can do the clean up (especially for the shortcut)
+//
+void commandMenuCleanUp()
+{
+	// Don't forget to deallocate your shortcut here
+}
+
+
+//
+// This function help you to initialize your plugin commands
+//
+bool setCommand(size_t index, TCHAR *cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey *sk, bool check0nInit) 
+{
+    if (index >= nbFunc)
+        return false;
+
+    if (!pFunc)
+        return false;
+
+    lstrcpy(funcItem[index]._itemName, cmdName);
+    funcItem[index]._pFunc = pFunc;
+    funcItem[index]._init2Check = check0nInit;
+    funcItem[index]._pShKey = sk;
+
+    return true;
+}
+
+//----------------------------------------------//
+//-- STEP 4. DEFINE YOUR ASSOCIATED FUNCTIONS --//
+//----------------------------------------------//
+//
+// もうひとつキー入力を受けて、それに割り当てられた処理をする。
+//
+void SubShortCut ()
+{
+    // Open a new document
+    ::SendMessage(nppData._nppHandle, NPPM_MENUCOMMAND, 0, IDM_FILE_NEW);
+
+    // Get the current scintilla
+    int which = -1;
+    ::SendMessage(nppData._nppHandle, NPPM_GETCURRENTSCINTILLA, 0, (LPARAM)&which);
+    if (which == -1)
+        return;
+    HWND curScintilla = (which == 0)?nppData._scintillaMainHandle:nppData._scintillaSecondHandle;
+
+    // Say hello now :
+    // Scintilla control has no Unicode mode, so we use (char *) here
+    ::SendMessage(curScintilla, SCI_SETTEXT, 0, (LPARAM)"SubShortCut");
+}
+
+
+
